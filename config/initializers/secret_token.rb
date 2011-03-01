ea89851c8dd9f80e18135af91a9b18e36451bb47
Rebecca Nesson <rebecca@prx.org>
2011-02-28 20:54:03 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Marketlist::Application.config.secret_token = '837fd011203d089c8dc3c883a242a6ce7511390943e2c082d542caf7f5348b745373e22b2ab90208ca35c0915f19ecf63ef9e283370d891b2033b1485c7e8b17'
