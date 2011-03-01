ae22651676193f622b72e8a44b8b47d10a69beaa
Rebecca Nesson <rebecca@prx.org>
2011-03-01 13:37:15 -0500

    working on page layouts and few other things

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Marketlist::Application.routes.draw do
+  
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+
   resources :products
 
   # The priority is based upon order of creation:
@@ -50,7 +55,7 @@ Marketlist::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "pages#home"
 
   # See how all your routes lay out with "rake routes"
 
---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,46 @@
+require 'spec_helper'
+
+describe PagesController do
+  render_views
+  
+  describe "GET 'home'" do
+    it "should be successful" do
+      get 'home'
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title",
+                        :content => "| Home")
+    end
+  end
+
+  describe "GET 'contact'" do
+    it "should be successful" do
+      get 'contact'
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title",
+                        :content => "| Contact")
+    end
+  end
+  
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title",
+                        :content => "| About")
+    end
+  end
+  
+
+end
