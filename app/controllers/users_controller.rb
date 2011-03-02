fc84bbb841022b05c75c6787998f216963525037
Rebecca Nesson <rebecca@prx.org>
2011-03-02 11:15:58 -0500

    working on user sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,23 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
     @title = "Sign Up"
   end
   
   def show
     @user = User.find(params[:id])
     @title = "#{@user.name}"
+  end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to the Market List!"
+      redirect_to @user
+    else
+      @title = "Sign up"
+      render 'new'
+    end
   end
 
 end
