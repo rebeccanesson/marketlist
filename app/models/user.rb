c3ed5b5fa7c9b9e716047a5c5362f64fde211b1b
Rebecca Nesson <rebecca@prx.org>
2011-03-03 21:24:31 -0500

    trying to get user updating to work without destroying passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -51,6 +51,7 @@ class User < ActiveRecord::Base
   private
 
   def encrypt_password
+    return if password.blank? 
     self.salt = make_salt if new_record?
     self.encrypted_password = encrypt(password)
   end
